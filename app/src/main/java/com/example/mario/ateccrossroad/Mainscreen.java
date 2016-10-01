49be88f658280cf37024181d839c3cccb849397c
mario <admin@marionobre.com>
2016-10-01 15:54:03 +0100

    Final noob version

--- app/src/main/java/com/example/mario/ateccrossroad/Mainscreen.java
+++ app/src/main/java/com/example/mario/ateccrossroad/Mainscreen.java
@@ -1,163 +1,287 @@
 package com.example.mario.ateccrossroad;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Mainscreen extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+    public int whereIs, points;
+    public boolean gameOn;
+    static Rect r1 = new Rect();
+    static Rect r2 = new Rect();
+    static ImageView img_animation;
+    AnimationDrawable rocketAnimation;
+    MediaPlayer gameSong, brsound;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_mainscreen);
+        newGame();
+    }
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
+    public void newGame(){
+        whereIs=0;
+        gameOn=true;
+        img_animation = (ImageView) findViewById(R.id.player);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(img_animation, "translationX", 0f, 550f);
+        animation.start();
+        ObjectAnimator anim = ObjectAnimator.ofFloat(img_animation, "translationy", 0f, 0f);
+        anim.start();
+        cars();
+        gameSong = MediaPlayer.create(this, R.raw.song);
+        brsound = MediaPlayer.create( this, R.raw.breaksound);
+        gameSong.start();
+        img_animation.setBackgroundResource(R.drawable.playeranime);
+        rocketAnimation = (AnimationDrawable) img_animation.getBackground();
+        rocketAnimation.start();
+    }
 
+    public void cars(){
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        final Timer timer = new Timer();
+        TimerTask timerTask;
+        timerTask = new TimerTask() {
             @Override
-            public void onClick(View view) {
-                toggle();
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (gameOn==false){
+                            timer.cancel();
+                            timer.purge();
+                        }
+                        newCarup();
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 2000);
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        newCardown();
+                        if (gameOn==false){
+                            timer.cancel();
+                            timer.purge();
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 1500);
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        animateStreet();
+                        if (gameOn==false){
+                            timer.cancel();
+                            timer.purge();
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 0, 2500);
+    }
+
+    public void move(View view){
+
+        ObjectAnimator animation;
+
+        switch (whereIs){
+
+            case 0:
+                animation = ObjectAnimator.ofFloat(img_animation, "translationY", 0f, 100f);
+                animation.start();
+                animation.setDuration(500);
+                whereIs++;
+                break;
+
+            case 1:
+                animation = ObjectAnimator.ofFloat(img_animation, "translationY", 100f, 250f);
+                animation.start();
+                animation.setDuration(500);
+                whereIs++;
+                break;
+
+            case 2:
+                animation = ObjectAnimator.ofFloat(img_animation, "translationY", 250f, 400f);
+                animation.start();
+                animation.setDuration(500);
+                whereIs++;
+                break;
+
+            case 3:
+                animation = ObjectAnimator.ofFloat(img_animation, "translationY", 400f, 250f);
+                animation.start();
+                animation.setDuration(500);
+                whereIs++;
+                break;
+
+            case 4:
+                animation = ObjectAnimator.ofFloat(img_animation, "translationY", 250f, 100f);
+                animation.start();
+                animation.setDuration(500);
+                whereIs++;
+                break;
+
+            case 5:
+                animation = ObjectAnimator.ofFloat(img_animation, "translationY", 100f, 0f);
+                animation.start();
+                animation.setDuration(500);
+                points++;
+                whereIs=0;
+                break;
+        }
+
+    }
+
+    public void animateStreet(){
+
+        ImageView rocketImage = (ImageView) findViewById(R.id.imv);
+        rocketImage.setBackgroundResource(R.drawable.valuesnim);
+        rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
+        ObjectAnimator positionchange = ObjectAnimator.ofFloat(rocketImage, "translationY", 0f, 0f);
+        positionchange.start();
+        ObjectAnimator animation = ObjectAnimator.ofFloat(rocketImage, "translationX", 1200f, -40f);
+        animation.start();
+        animation.setDuration(2000);
+        rocketAnimation.start();
+    }
+
+    public void newCarup(){
+        final ImageView iv = new ImageView(this);
+        iv.setImageResource(R.drawable.car1);
+        FrameLayout fl = (FrameLayout) findViewById(R.id.main);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+        fl.addView(iv,lp);
+        ObjectAnimator positionchange = ObjectAnimator.ofFloat(iv, "translationY", 200f, 200f);
+        positionchange.start();
+        final ObjectAnimator animation = ObjectAnimator.ofFloat(iv, "translationX", -400f, 2000f);
+        animation.start();
+        animation.setDuration(7000);
+        final Context context = this;
+
+        animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                r1.set((int)iv.getTranslationX(), (int)iv.getTranslationY(), ((int)iv.getTranslationX()+100), ((int)iv.getTranslationY()+50));
+                r2.set((int)img_animation.getTranslationX(), (int)img_animation.getTranslationY(), ((int)img_animation.getTranslationX()+100), ((int)img_animation.getTranslationY()+200));
+                if(Rect.intersects(r1, r2) && gameOn==true)
+                {
+                    gameOn=false;
+                    gameSong.stop();
+                    brsound.start();
+
+                    new AlertDialog.Builder(context)
+                            .setTitle("Sanguinetti was Wrecked!")
+                            .setMessage("He was able to develop Bubbly Invasion but he couldn't cross the road!\nYou scored "+points+" points!\nWould you like to play again?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    newGame();
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                                    intent.addCategory(Intent.CATEGORY_HOME);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                    /* TESTE DE COLISAO
+                    System.out.println(""+iv.getTranslationX()+" "+iv.getTranslationY()+" "+((int)iv.getTranslationX()+100)+" "+((int)iv.getTranslationY()+50)+" CAMION");
+                    System.out.println(""+img_animation.getTranslationX()+" "+img_animation.getTranslationY()+" "+((int)img_animation.getTranslationX()+100)+" "+((int)img_animation.getTranslationY()+100)+" SANGUI");
+                    Toast.makeText(MainActivity.this, "PERDEU", Toast.LENGTH_SHORT).show();
+                    */
+                }
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    public void newCardown(){
+        final ImageView iv = new ImageView(this);
+        iv.setImageResource(R.drawable.car2);
+        final ObjectAnimator animation;
+        FrameLayout fl = (FrameLayout) findViewById(R.id.main);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,FrameLayout.LayoutParams.WRAP_CONTENT);
+        fl.addView(iv,lp);
+        ObjectAnimator positionchange = ObjectAnimator.ofFloat(iv, "translationY", 300f, 300f);
+        positionchange.start();
+        animation = ObjectAnimator.ofFloat(iv, "translationX", 2000f, -400f);
+        animation.start();
+        animation.setDuration(7000);
+        final Context context = this;
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+        animation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+
+                r1.set((int)iv.getTranslationX(), (int)iv.getTranslationY(), ((int)iv.getTranslationX()+100), ((int)iv.getTranslationY()+50));
+                r2.set((int)img_animation.getTranslationX(), (int)img_animation.getTranslationY(), ((int)img_animation.getTranslationX()+100), ((int)img_animation.getTranslationY()+200));
+                if(Rect.intersects(r1, r2) && gameOn==true)
+                {
+                    gameOn=false;
+                    gameSong.stop();
+                    brsound.start();
+
+                    new AlertDialog.Builder(context)
+                            .setTitle("Sanguinetti was Wrecked!")
+                            .setMessage("He was able to develop Bubbly Invasion but he couldn't cross the road!\nYou scored "+points+" points!\nWould you like to play again?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    newGame();
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                                    intent.addCategory(Intent.CATEGORY_HOME);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            }
+        });
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
 
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
 }
