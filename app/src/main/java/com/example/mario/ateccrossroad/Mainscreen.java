74d82e499e31595e6310c501a95e01ce7c959aad
Antonio Silva <antnunessilva@gmail.com>
2016-10-01 23:48:27 +0100

    Initial screen

--- app/src/main/java/com/example/mario/ateccrossroad/Mainscreen.java
+++ app/src/main/java/com/example/mario/ateccrossroad/Mainscreen.java
@@ -58,6 +58,8 @@ public class Mainscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String escolha  = intent.getStringExtra("Escolha"); //resultado da escolha
         newGame();
 
     }
